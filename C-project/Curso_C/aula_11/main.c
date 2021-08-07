52425136afcd9e226cdd7567b0d7d6c1c9e35dd6
HeimdallSec <eduardo.oliverti@gmail.com>
2021-08-07 20:26:32 -0300

    project add

--- C-project/Curso_C/aula_11/main.c
+++ C-project/Curso_C/aula_11/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void imprime(char *s) {
+    while (*s) {
+        putchar(*s++);
+    }
+}
 
 int main(void) {
-    char *p = "c";
+    char u[] = {0x66, 0x65, 0x72, 0x6e, 0x61, 0x6e, 0x64, 0x6f, '\n', 0x00};
 
-    printf("%p", p);
+    // puts(u);
+
+    imprime(u);
 
     return 0;
 }
