e50baa76b0d0291eb5d9be60670bd94137331497
HeimdallSec <eduardo.oliverti@gmail.com>
2021-08-08 02:17:35 -0300

    add code

--- C-project/Curso_C/aula_11/main.c
+++ C-project/Curso_C/aula_11/main.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void imprime(char *s) {
-    while (*s) {
-        putchar(*s++);
-    }
-}
+// int argc == Quantidade de argumentos
+//     Variável do tipo inteiro
+// char *argv[] == Argumentos da função
+//     Ponteiro para string (Array/Ponteiro de caracteres)
+// char *envp[] == Variáveis de ambiente
+//     Ponteiro para string (Array/Ponteiro de caracteres)
 
-int main(void) {
-    char u[] = {0x66, 0x65, 0x72, 0x6e, 0x61, 0x6e, 0x64, 0x6f, '\n', 0x00};
+int main(int argc, char *argv[]) {
 
-    // puts(u);
+    // if (argc < 2) {
+    //     printf("Digite um ip!\n");
+    // }
+    // else if (argc > 2) {
+    //     printf("Somente suporto um ip, seu doido!");
+    // }
 
-    imprime(u);
+    int i; // 4 bytes alocados para um int32
+    printf("endereco de i: %p / tamanho de i: %zu\n", &i, sizeof i);
+
+    int *p;
+    printf("endereco de p: %p / tamanho de p: %zu\n", &p, sizeof p);
+    
+    *p = &i; //
+
+    *p = 9;
+
+    printf("%zu\n", sizeof(int *));
 
     return 0;
 }
